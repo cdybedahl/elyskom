1559f47d283506f2ecac25b77613d93e20ffc41a
Calle Dybedahl <github@calle.dybedahl.se>
2021-06-02 20:50:19 +0200

    Shorten that a bit.

--- src/elyskom_call.erl
+++ src/elyskom_call.erl
@@ -3,10 +3,9 @@
 -export([make/2]).
 
 make(login, Args) ->
-    Pairs = lists:zip([prot_a_integer, prot_a_integer, prot_a_string, prot_a_bool], [62 | Args]),
-    Parts = process(Pairs),
-    Parts.
+    process([prot_a_integer, prot_a_integer, prot_a_string, prot_a_bool], [62 | Args]).
 
-process(List) ->
+process(Pattern, Pairs) ->
+    List = lists:zip(Pattern, Pairs),
     Elements = lists:map(fun({Type, Data}) -> Type:encode(Data) end, List),
     lists:join(<<" ">>, Elements).
