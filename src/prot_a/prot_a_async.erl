d1c2900172f1affcf48e1996b9c7281c25e2585c
Calle Dybedahl <github@calle.dybedahl.se>
2021-06-04 20:33:56 +0200

    Asyncs 9 and 13.

--- src/prot_a/prot_a_async.erl
+++ src/prot_a/prot_a_async.erl
@@ -2,8 +2,16 @@
 
 -export([parse/1]).
 
-parse([<<"7">>]) ->
-    async_saving_database;
 parse(List) ->
-    logger:error("Unknown async: ~p", [List]),
-    async_wtf_is_that.
+    do_parse(List).
+
+do_parse([<<"7">>]) ->
+    async_saving_database;
+do_parse([<<"9">> | Tail]) ->
+    [PersNo, SessionNo] = prot_a_args:parse([prot_a_integer, prot_a_integer], Tail),
+    {async_login, PersNo, SessionNo};
+do_parse([<<"13">> | Tail]) ->
+    [PersNo, SessionNo] = prot_a_args:parse([prot_a_integer, prot_a_integer], Tail),
+    {async_logout, PersNo, SessionNo};
+do_parse(List) ->
+    {async_wtf_is_that, List}.
