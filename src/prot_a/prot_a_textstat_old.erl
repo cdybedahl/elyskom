69a7ec43dc1912f2d11d91a32b272cbd6b976e44
Calle Dybedahl <calle.dybedahl@kivra.com>
2021-06-08 15:20:38 +0200

    Cleaner.

--- src/prot_a/prot_a_textstat_old.erl
+++ src/prot_a/prot_a_textstat_old.erl
@@ -5,17 +5,26 @@
 -export([parse/1]).
 
 parse(List) ->
-    {CreationTime, Rest0} = prot_a_time:parse(List),
-    {Author, Rest1} = prot_a_integer:parse(Rest0),
-    {NoOfLines, Rest2} = prot_a_integer:parse(Rest1),
-    {NoOfChars, Rest3} = prot_a_integer:parse(Rest2),
-    {NoOfMarks, Rest4} = prot_a_integer:parse(Rest3),
-    {MiscInfo, Rest5} = prot_a_array:parse(Rest4, prot_a_misc_info),
-    {#{
-        creation_time => CreationTime,
-        author => Author,
-        no_of_lines => NoOfLines,
-        no_of_chars => NoOfChars,
-        no_of_marks => NoOfMarks,
-        misc_info => MiscInfo
-    }, Rest5}.
+    {[CreationTime, Author, NoOfLines, NoOfChars, NoOfMarks, MiscInfo], Rest} =
+        prot_a_args:get(
+            [
+                prot_a_time,
+                prot_a_integer,
+                prot_a_integer,
+                prot_a_integer,
+                prot_a_integer,
+                [prot_a_misc_info]
+            ],
+            List
+        ),
+    {
+        #{
+            creation_time => CreationTime,
+            author => Author,
+            no_of_lines => NoOfLines,
+            no_of_chars => NoOfChars,
+            no_of_marks => NoOfMarks,
+            misc_info => MiscInfo
+        },
+        Rest
+    }.
