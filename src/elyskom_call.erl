bde435f5058ad3f34d661e6a98db6b342c31e7a7
Calle Dybedahl <github@calle.dybedahl.se>
2021-06-12 14:12:49 +0200

    Refactor to better separate unique info from common structure.

--- src/elyskom_call.erl
+++ src/elyskom_call.erl
@@ -4,18 +4,9 @@
 
 -export([make/2]).
 
-make(get_time, []) ->
-    process([], [35]);
-make(get_text, Args) ->
-    process([prot_a_integer, prot_a_integer, prot_a_integer], [ 25 | Args]);
-make(get_uconf_stat, Args) ->
-    process([prot_a_integer], [ 78 | Args]);
-make(login, Args) ->
-    process([prot_a_integer, prot_a_string, prot_a_bool], [62 | Args]);
-make(accept_async, AsyncList) ->
-    process([[prot_a_integer]], [80 | AsyncList]).
-
-process(Pattern, Pairs) ->
+make(CallName, Args) ->
+    Pattern = make_args(CallName),
+    Pairs = [call_no(CallName) | Args],
     Elements = lists:map(
         fun
             ({[Type], Data}) ->
@@ -26,6 +17,18 @@ process(Pattern, Pairs) ->
         lists:zip([prot_a_integer | Pattern], Pairs)
     ),
     lists:join(<<" ">>, Elements).
+
+call_no(get_time) -> 35;
+call_no(get_text) -> 25;
+call_no(get_uconf_stat) -> 78;
+call_no(login) -> 62;
+call_no(accept_async) -> 80.
+
+make_args(get_time) -> [];
+make_args(get_text) -> [prot_a_integer, prot_a_integer, prot_a_integer];
+make_args(get_uconf_stat) -> [prot_a_integer];
+make_args(login) -> [prot_a_integer, prot_a_string, prot_a_bool];
+make_args(accept_async) -> [[prot_a_integer]].
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
