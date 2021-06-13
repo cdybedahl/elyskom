cea644702148661f6f62fe16de0d2db410e6da5d
Calle Dybedahl <github@calle.dybedahl.se>
2021-06-13 15:00:19 +0200

    dynamic session info

---
+++ src/prot_a/prot_a_dynamic_session_info.erl
@@ -0,0 +1,24 @@
+-module(prot_a_dynamic_session_info).
+
+-export([parse/1]).
+
+parse(List) ->
+    {[Session, Person, Conference, IdleTime, RawFlags, What], Rest} = prot_a_args:get(
+        [prot_a_integer, prot_a_integer, prot_a_integer, prot_a_integer, prot_a_bitstring, prot_a_string],
+        List
+    ),
+    Flags = parse_a_bitstring:annotate(
+        [invisible, user_active_used, user_absent, reserved3, reserved4, reserved5, reserved6, reserved7],
+        RawFlags
+    ),
+    {
+        #{
+            session_no => Session,
+            person => Person,
+            working_conference => Conference,
+            idle_time => IdleTime,
+            flags => Flags,
+            what_am_i_doing => What
+        },
+        Rest
+    }.
