17b8d9592d528fb90f9262332d77c2064aa17993
Calle Dybedahl <calle.dybedahl@kivra.com>
2021-06-28 09:21:40 +0200

    Hm. Was this not commited?

--- src/prot_a/prot_a_string.erl
+++ src/prot_a/prot_a_string.erl
@@ -15,10 +15,12 @@ parse([Str | Tail]) ->
 encode(List) when is_list(List) ->
     encode(iolist_to_binary(List));
 encode(String) when is_binary(String) ->
-    case unicode:characters_to_binary(String, utf8, latin1) of
-        Utf8 when is_binary(Utf8) ->
-            Len = integer_to_binary(byte_size(Utf8)),
-            <<Len/binary, "H", Utf8/binary>>;
-        {error, _, _} ->
-            throw(io_lib:format("Could not convert to Latin-1: ~tp", [String]))
-    end.
+    Payload =
+        case unicode:characters_to_binary(String, utf8, latin1) of
+            Converted when is_binary(Converted) ->
+                Converted;
+            {error, _, _} ->
+                String
+        end,
+    Len = integer_to_binary(byte_size(Payload)),
+    <<Len/binary, "H", Payload/binary>>.
